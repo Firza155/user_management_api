97e18f85575bb7f29f92d005e24cb9063bb26996
Firza155 <cobalagi567@gmail.com>
2025-10-22 22:50:04 +0700

    menambahkan konfigurasi server express dengan keamanan CORS dan Helmet

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import authRoutes from './src/routes/authRoutes.js';
+import userRoutes from './src/routes/userRoutes.js';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+})); 
+app.use(helmet());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);  
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log ('Server berjalan di port ${PORT}');
+});
